305ba7a21ae3361413b46773fc8c75a1fbff7aed
molnarteodor <60893310+molnarteodor@users.noreply.github.com>
2020-02-24 14:58:30 +0200

    Update ListStudent.java

--- src/homework/ListStudent.java
+++ src/homework/ListStudent.java
@@ -6,7 +6,7 @@ public class ListStudent {
     public static void main(String[] args) throws Exception {
         String name = null;
         int grade = 0;
-        StudentClass reader = new StudentClass(name, grade);
+        Student reader = new Student(name, grade);
         List<Student> students = reader.readFile("grades.txt");
         System.out.println(students);
     }
